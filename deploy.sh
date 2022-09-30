5eac16b959c4852726f136fb3f324c5b08007e5f
Jonathan Lott <921121+jlott1@users.noreply.github.com>
2022-09-30 16:36:26 -0500

    adding debug logging

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,13 @@ source /root/.bashrc
 
 /app/script.exp $2
 
+echo "pwd ."
+pwd .
+echo "ls -la /app"
+ls -la /app
+echo "ls -la ."
+ls -la .
+echo "b4a default $1"
 b4a default "$1"
-ls -l .
+echo "b4a deploy"
 b4a deploy
