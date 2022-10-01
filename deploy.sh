0cc4f06752f10f2a672bd8c98736473efdf12917
Jonathan Lott <921121+jlott1@users.noreply.github.com>
2022-10-01 02:17:01 -0500

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,18 +4,18 @@ source /root/.bashrc
 
 /app/script.exp $2
 
-echo "ls -la ../"
-ls -la ../
-echo "ls -la ../home"
-ls -la ../home
-echo "ls -la ../workflow"
-ls -la ../workflow
-echo "pwd ."
-pwd .
-echo "ls -la /app"
-ls -la /app
-echo "ls -la ."
-ls -la .
+#echo "ls -la ../"
+#ls -la ../
+#echo "ls -la ../home"
+#ls -la ../home
+#echo "ls -la ../workflow"
+#ls -la ../workflow
+#echo "pwd ."
+#pwd .
+#echo "ls -la /app"
+#ls -la /app
+#echo "ls -la ."
+#ls -la .
 echo "b4a default $1"
 b4a default "$1"
 echo "b4a deploy"
